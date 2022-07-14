21a3bbd303ebdeee0e95617e0ea95571dcef6bb3
SAMETAVCII <you@example.com>
2022-07-14 12:32:29 +0300

    Bubble sort added

---
+++ sort-algorithm/bubblesort.go
@@ -0,0 +1,12 @@
+package sort_algorithm
+
+func BubbleSort(array []int) []int {
+	for i := 0; i < len(array)-1; i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+	return array
+}
