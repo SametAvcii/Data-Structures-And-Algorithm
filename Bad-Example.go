144009c9c867dacebcf8efeeac2d9db828bd9cfd
sametavcii <sametavc05@gmail.com>
2022-08-03 20:48:45 +0300

    Solid added

---
+++ Bad-Example.go
@@ -0,0 +1,17 @@
+import "github.com/dgrijalva/jwt-go"
+
+func extractUsername(header http.Header) string {
+	raw := header.Get("Authorization")
+	parser := &jwt.Parser{}
+	token, _, err := parser.ParseUnverified(raw, jwt.MapClaims{})
+	if err != nil {
+		return ""
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return ""
+	}
+
+	return claims["username"].(string)
+}
