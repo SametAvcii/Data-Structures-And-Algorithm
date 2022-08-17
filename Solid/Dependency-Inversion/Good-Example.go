0230d4481cd81e87b103219b582d9de4f19e55d2
sametavcii <sametavc05@gmail.com>
2022-08-18 01:43:05 +0300

    Dependecy Inversion added

--- Solid/Dependency-Inversion/Good-Example.go
+++ Solid/Dependency-Inversion/Good-Example.go
@@ -2,76 +2,6 @@ package solid
 
 import "fmt"
 
-/*package goodCode2
-
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"os"
-)
-
-type User struct {
-	ID int
-}
-
-type UserJSON struct {
-	ID int `json:"id"`
-}
-
-func (j UserJSON) ToUser() *User {
-	return &User{
-		ID:j.ID,
-	}
-}
-
-func GetUserFile(id uint) (io.Reader, error) {
-	filename := fmt.Sprintf("user_%d.json", id)
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	return file, nil
-}
-
-func GetUserHTTP(id uint) (io.Reader, error) {
-	uri := fmt.Sprintf("github.com/sametavcii/%d", id)
-	resp, err := http.Get(uri)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.Body, nil
-}
-
-func GetDummyUser(userJSON UserJSON) (io.Reader, error) {
-	data, err := json.Marshal(userJSON)
-	if err != nil {
-		return nil, err
-	}
-
-	return bytes.NewReader(data), nil
-}
-
-func GetUser(reader io.Reader) (*User, error) {
-	data, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-
-	var user UserJSON
-	err = json.Unmarshal(data, &user)
-	if err != nil {
-		return nil, err
-	}
-
-	return user.ToUser(), nil
-}*/
-
 type User struct {
 	ID   string
 	Name string
@@ -85,7 +15,6 @@ type Storage struct {
 func NewStorage() *Storage {
 	return &Storage{
 		items: make(map[string]interface{}),
-		
 	}
 }
 
@@ -99,7 +28,7 @@ func (s *Storage) Add(key string, item interface{}) error {
 func (s *Storage) Get(key string) (interface{}, error) {
 	user, ok := s.items[key]
 	if !ok {
-		return nil,  fmt.Errorf("not found")
+		return nil, fmt.Errorf("not found")
 	}
 	return user, nil
 }
@@ -113,7 +42,6 @@ func (s *Storage) Delete(key string) (interface{}, error) {
 	delete(s.items, key)
 	return item, nil
 }
-
 
 type userStorage interface {
 	add(key string, item interface{}) error
