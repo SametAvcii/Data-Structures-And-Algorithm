cd6c7c9e54e096381646bfe66b5edaf98eddfd6b
sametavcii <sametavc05@gmail.com>
2022-10-10 23:53:25 +0300

    Hash algorithm updated

--- HashExample/main.go
+++ HashExample/main.go
@@ -2,62 +2,138 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	pass := "abcdefgh"
+	pass := "abcd12A."
+
 	hashPass := Hash(pass)
+	decPass := DecyrptHash(hashPass)
 
 	fmt.Println("hashed:", hashPass)
-	fmt.Println(DecyrptHash(hashPass))
+
+	fmt.Println("decPass:",decPass)
 
 }
 
-func Hash(pass string) []int {
+func Hash(pass string) string {
 
-	arr := Character()
+	passArr := strings.Split(pass, "")
+	lenPass := len(passArr)
+	fmt.Println(lenPass)
 
-	hashMap := make(map[string]int, len(arr))
+	var arr []string
+	for _, passChar := range passArr {
 
-	for index, char := range arr {
-		hashMap[char] = index
+		value := ""
+		asciiValue := []byte(passChar)
+		sumDigit, count := findDigitSum(int(asciiValue[0]))
+
+		intAscii := int(asciiValue[0])
+
+		digits := strings.Split(strconv.Itoa(intAscii), "")
+		firstDigit := digits[0]
+		if len(digits)==3{
+			firstDigit+=digits[1]
+		}
+
+		value += firstDigit + strconv.Itoa(sumDigit) + strconv.Itoa(count)
+		arr = append(arr, value)
+
 	}
 
-	splitPass := strings.Split(pass, "")
 
-	var hashPass []int
-	for _, char := range splitPass {
-		value := hashMap[char]
-		hashPass = append(hashPass, value)
+	hashValue := ""
+
+	for _, val := range arr {
+
+		hashValue += "23" + val
 	}
-
-	return hashPass
+	return hashValue
+}
+func findDigitSum(num int) (int, int) {
+	res := 0
+	count := 0
+	for num > 0 {
+		res += num % 10
+		num /= 10
+		count++
+	}
+	return res, count
 }
 
-func DecyrptHash(pass []int) string {
+func DecyrptHash(pass string) string {
 
-	arr := Character()
+	splitArr := strings.Split(pass, "23")
+	splitArr=splitArr[1:]
 
-	hashMap := make(map[int]string, len(arr))
 
-	for index, char := range arr {
-		hashMap[index] = char
+	var asciiArr []int
+
+	for _, val := range splitArr {
+	var ascVal string
+		
+		digit:= strings.Split(val, "")
+
+		ascVal += digit[0]
+
+		lastIndis := len(digit) - 1
+		if digit[(lastIndis)] == "3" {
+
+			ascVal += digit[1]
+			digit0, _ := strconv.Atoi(digit[0]) //100
+			digit1, _ := strconv.Atoi(digit[1])
+
+			digit = digit[2 : len(digit)-1]
+			var total string
+
+			for _, tot := range digit {
+				total += tot
+			}
+
+			tot, _ := strconv.Atoi(total)
+			thirdDigit := tot - (digit0 + digit1)
+			thirdDigitStr := strconv.Itoa(thirdDigit)
+			ascVal += thirdDigitStr
+		} else if digit[(lastIndis)] == "2" {
+			digit0, _ := strconv.Atoi(digit[0])
+
+			digit = digit[1 : len(digit)-1]
+			var total string
+
+			for _, tot := range digit {
+				total += tot
+			}
+
+			tot, _ := strconv.Atoi(total)
+			secDigit := tot - digit0
+
+			secDigitStr := strconv.Itoa(secDigit)
+
+			ascVal += secDigitStr
+		} else {
+			ascVal += digit[0]
+		}
+
+		indis, _ := strconv.Atoi(ascVal)
+		asciiArr = append(asciiArr, indis)
 	}
-	var decyrptPass string
-	for _, char := range pass {
-		value := hashMap[char]
+	
+	
+	var decHash string
+	for _, ascii := range asciiArr {
 
-		decyrptPass = decyrptPass + value
+		decHash+=string(ascii)
 	}
-	return decyrptPass
+
+	return decHash
 
 }
-
-func Character() []string {
-	arr := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	arr = append(arr, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
-	arr = append(arr, "1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
-
-	return arr
+func reverse(str string) (result string) {
+    for _, v := range str {
+        result = string(v) + result
+    }
+    return
 }
